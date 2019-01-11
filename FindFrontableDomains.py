f276abf0875dda98ffd08bef08f99babde4bf37a
rvrsh3ll <rvrsh3ll@gmail.com>
2019-01-11 11:53:51 -0500

    Fixed

--- FindFrontableDomains.py
+++ FindFrontableDomains.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 # Run setup.sh first!
 
-import alexa
-import dns.resolver
-from dns.resolver import Resolver
+import dns
 import threading
 import Queue
 import argparse
@@ -23,7 +21,7 @@ class ThreadLookup(threading.Thread):
             hostname = self.queue.get()
             try:
 
-                dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+                dns.resolver.default_resolver = dns.Resolver(configure=False)
                 dns.resolver.default_resolver.nameservers = ['209.244.0.3', '209.244.0.4','64.6.64.6','64.6.65.6', '8.8.8.8', '8.8.4.4','84.200.69.80', '84.200.70.40', '8.26.56.26', '8.20.247.20', '208.67.222.222', '208.67.220.220','199.85.126.10', '199.85.127.10', '81.218.119.11', '209.88.198.133', '195.46.39.39', '195.46.39.40', '96.90.175.167', '193.183.98.154','208.76.50.50', '208.76.51.51', '216.146.35.35', '216.146.36.36', '37.235.1.174', '37.235.1.177', '198.101.242.72', '23.253.163.53', '77.88.8.8', '77.88.8.1', '91.239.100.100', '89.233.43.71', '74.82.42.42', '109.69.8.51']
                 query = dns.resolver.query(hostname, 'a')
                 # Iterate through response and check for potential CNAMES
@@ -58,11 +56,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', type=str, required=False)
     parser.add_argument('-t', '--threads', type=int, required=False, default=10)
-    parser.add_argument('-a', '--alexa', type=int, required=False)
     parser.add_argument('-d', '--domain', type=str, required=False)
     args = parser.parse_args()
     threads =  args.threads
-    top = args.alexa
+
     file = args.file
     domain = args.domain
     queue = Queue.Queue()
@@ -78,13 +75,6 @@ def main():
         for i in subdomains:
             print i
             queue.put(i)
-    elif alexa:
-        results = alexa.top_list(top)
-        for i in results:
-            for r in i:
-                r = str(r)
-                if not r.isdigit():
-                    queue.put(r)
     else:
         print "No Input Detected!"
         sys.exit()
