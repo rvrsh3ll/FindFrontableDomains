29d5c59238af9ae6490a143df1d38bcdd04a586d
rvrsh3ll <rvrsh3ll@gmail.com>
2019-11-11 09:18:25 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-pip uninstall -y dnspython
-pip install -r requirements.txt
+pip3 uninstall -y dnspython
+pip3 install -r requirements.txt
 git clone https://github.com/rvrsh3ll/Sublist3r.git
+python3 Sublist3r/setup.py install
 touch Sublist3r/__init__.py
 exit 1
