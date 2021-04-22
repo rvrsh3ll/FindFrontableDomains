d90a9a1f1f85d45453aa4320b1bb428fc5155e3f
omr00t <om-root@hotmail.com>
2021-04-22 09:19:48 +0400

    Added virtual environment installation option

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+[[ $# -gt 0 && "--venv" =~ $@ ]] && venv=true || venv=false
+
+if [[ $EUID -ne 0 && $venv == "false" ]]; then
+	echo "This script must be either run as root or within a virtual environment." 1>&2
+	exit 1
 fi
+
 pip3 uninstall -y dnspython
 pip3 install -r requirements.txt
 git clone https://github.com/aboul3la/Sublist3r.git
